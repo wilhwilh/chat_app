bc2d580772c0a8660d493a3c4ce2767a82756a48
Wilhge <wilhge@gmail.com>
2019-06-16 18:47:05 -0400

    socketio, conecction and disconnection

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require("socket.io");
 
 const publicPath = path.join(__dirname, "/../public");
 const port = process.env.PORT || 3000;
-var app = express();
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(3000, ()=>{ 
+io.on('connection', (socket)=>{
+    console.log("A new user just conneced");
+
+    socket.on('disconnect', ()=>{
+        console.log('A user was disconnected from server.');
+    });
+    
+    
+})
+
+
+server.listen(3000, ()=>{ 
     console.log(`Server is up on port ${port}`); 
 
 });
